8c5655fac3b7c2bbfe717f133ba4b2b3f9539ae1
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-05-23 15:09:30 -0400

    Add product images and styling

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Products from './Products'
+import styles from './Homepage.module.css'
 
 function Homepage() {
   const [productCards, setProductCards] = useState([])
@@ -16,11 +17,19 @@ function Homepage() {
 
   return (
     <>
-      {productCards.map((item, index) => (
-        <div key={index}>
-          {item.title} {item.price}
-        </div>
-      ))}
+      <div className={styles.productGrid}>
+        {productCards.map((item, index) => (
+          <div key={index} className={styles.product}>
+            <img
+              className={styles.productImage}
+              src={item.image}
+              alt={item.description}
+            />
+            <p>{item.title}</p>
+            <p>{item.price}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
